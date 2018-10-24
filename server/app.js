326744d88d2ef64e5ee9b317984c2e861f0c7b97
Feyi AbrahamAdesanya <val.ezeh15@gmail.com>
2018-10-24 09:31:11 +0100

    chore(set up server): set up express server - Install application dependencies - Set-up express server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
+
+// Instance of the express app
+const app = express();
+
+// Request logger
+app.use(morgan('dev'));
+
+// Parsing body data
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.status(200).send({
+    message: 'Welcome to Developer Contact Directory',
+  });
+});
+
+
+const port = process.env.PORT || 7000;
+
+app.listen(port);
+
+console.log(`Find me on http://localhost:${port}`);
