d7bcdb531e953f4675cefd06c319822ee5219263
Feyi AbrahamAdesanya <val.ezeh15@gmail.com>
2018-10-24 09:43:51 +0100

    develop branch

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.status(200).send({
-    message: 'Welcome to Developer Contact Directory',
+    message: 'Welcome to Developer Contact Directory..',
   });
 });
 
